50a99518e538cd449768daae5cab9d0fdf3d6d28
caiomcg <caiomcg@gmail.com>
2018-05-25 17:35:31 -0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+sudo apt-get install i3 i3-wm i3status # i3
+
+sudo apt-get install rofi comptonls lxappearance feh # i3 support - add xrandr
+
+sudo apt-get install fonts-powerline fonts-font-awesome # fonts
+
+# Clone Bumblebee status 
+
+sudo pip install psutil
+sudo pip install netifaces
