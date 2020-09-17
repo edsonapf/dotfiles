c31c04a624e90265200efb5dcd0a4cc494520f63
caio <caio.marcelo@assistatecnologia.com.br>
2020-09-17 14:48:56 -0300

    Fix minor problems with install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,19 @@
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo add-apt-repository ppa:neovim-ppa/stable
     sudo apt-get update -y
-    sudo apt-get -y install zsh tmux git neovim
+    sudo apt-get -y install zsh tmux git neovim curl
 fi
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes
+mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 cp *.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes
 
 cp .zshrc ~/
 cp .tmux.conf ~/
-cp -r .config/ ~/`.config/`
+cp -r .config/ ~/.config/
