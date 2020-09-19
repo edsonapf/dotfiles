aa22dc5a717f6a9a2ae0586c21da0b48e2eaf321
caiomcg <caiomcg@gmail.com>
2020-09-19 15:51:32 -0300

    Fix installation order

--- install.sh
+++ install.sh
@@ -5,6 +5,12 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     sudo apt-get -y install zsh tmux git neovim curl
 fi
 
+cp .tmux.conf ~/
+
+cp -r .config/nvim ~/.config/
+cp -r .config/alacritty ~/.config/
+
+
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes
@@ -13,10 +19,5 @@ mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-cp *.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes
-
 cp .zshrc ~/
-cp .tmux.conf ~/
-
-cp -r .config/nvim ~/.config/
-cp -r .config/alacritty ~/.config/
+cp *.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes
