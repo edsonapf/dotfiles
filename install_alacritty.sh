f5a4898d3e39de7addafdffab5bcb0430c053c97
caiomcg <caiomcg@gmail.com>
2020-09-20 13:50:18 -0300

    Add extra installation scripts

---
+++ install_alacritty.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function compile() {
+    cd alacritty
+
+    cargo build --release
+
+    sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+    sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+    sudo desktop-file-install extra/linux/Alacritty.desktop
+    sudo update-desktop-database
+}
+
+sudo apt-get install -y cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python
+
+git clone --branch v0.5.0 https://github.com/alacritty/alacritty.git
+
+compile
+
---
+++ install_i3.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo add-apt-repository -y ppa:kgilmer/speed-ricer
+sudo apt update
+sudo apt install -y i3-gaps i3-gaps-wm compton polybar
