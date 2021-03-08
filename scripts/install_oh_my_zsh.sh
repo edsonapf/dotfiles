b48ad8cd25e0a69a8571aaffc90260072f1b22c9
caiomcg <caiomcg@gmail.com>
2021-03-07 21:33:54 -0300

    fix: Invalid path and folder copy

--- scripts/install_oh_my_zsh.sh
+++ scripts/install_oh_my_zsh.sh
@@ -8,6 +8,6 @@ mkdir -p ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-git clone https://github.com/caiomcg/gruvbox-zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes
-curl -o - https://raw.githubusercontent.com/denysdovhan/spaceship-zsh-theme/master/install.zsh | zsh
+git clone https://github.com/caiomcg/gruvbox-zsh-theme /tmp/gruvbox-zsh-theme
+cp -r /tmp/gruvbox-zsh-theme/*.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/
 
